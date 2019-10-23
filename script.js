eea898bd7b5a23784ce79b7b6f45dfba0824e297
Glitch (hello-webpage) <none>
2019-10-23 15:26:35 +0000

    👶🐟 Checkpoint ./script.js:1578515/980

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let interval
-let colors = ["white", "red", "orange", "yellow", "green", "blue", "darkblue", "purple"]
+const colors = ["white", "red", "orange", "yellow", "green", "blue", "darkblue", "purple"]
+const keywords = ['whee', 'hui']
 function oida() {
   return Array(colors.length).fill(25).map((n, i) => n * (i + 1) / 100).map(n => `${n}rem `.repeat(2)).map((x, i) => `${x}${colors[i]}`).join(', ')
 }
+
+const getParams = () => new URLSearchParams(location.search)
 console.log(colors)
-console.log(oida)
 
 function whee() {
-  interval = setInterval(() => {
-    colors = [ colors[0], ...colors.slice(2), colors[1] ]
-    document.querySelector('body').style.textShadow = oida()
-  }, 100)
+  if (!interval) {
+    interval = setInterval(() => {
+      colors = [ colors[0], ...colors.slice(2), colors[1] ]
+      document.querySelector('body').style.textShadow = oida()
+    }, 100)
+  }
 }
 
 const makeLine = text => {
@@ -28,16 +32,24 @@ if (location.hash) {
 }
 
 if (location.search) {
-  const params = new URLSearchParams(location.search)
+  const params = getParams()
   if (params.get('whee') !== null) whee()
   document.body.style.fontSize = params.get('size')
   document.body.style.background = params.get('background')
   document.body.style.color = params.get('color')
-  colors[0] = params.get('shadow')
+  if (params.get('shadow') !== null) colors[0] = params.get('shadow')
 }
 
 const update = () => setTimeout(() => {
   location.hash = encodeURIComponent(document.body.innerText)
+  if (keywords.some(k => document.body.innerText.includes(k))) {
+    const params = getParams()
+    if (params.get('whee') === null) {
+      whee()
+      location.search = (location.search.length === 0 ? '' : '&')
+        + 'whee'
+    }
+  }
 })
 
 const throttledUpdate = _.debounce(update, 500, { trailing: true })
